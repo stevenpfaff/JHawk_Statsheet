e2dac94f8bf8e2719ab1d0b45702c23198909ab0
stevenpfaff <pfaffy1426@gmail.com>
2022-04-10 12:14:05 -0500

    added away locations for league games

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -6,7 +6,7 @@ function Schedule() {
     return (
         <div style={{ marginRight: "15%", marginLeft: "15%", marginBottom: "10%" }} >
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <h1 style={{ marginRight: "10%", marginLeft: "10%", marginBottom: "5%", marginTop: "5%", fontFamily: "inherit" }}>Team Schedule</h1>
+            <h1 style={{ marginRight: "10%", marginLeft: "10%", marginBottom: "5%", marginTop: "5%", fontFamily: "inherit" }}>League Schedule</h1>
             <Table striped bordered hover>
                 <thead>
                     <tr>
